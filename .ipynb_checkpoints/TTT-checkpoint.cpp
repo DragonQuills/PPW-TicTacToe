3a7d2a5d1af5fa64dd020f16ff0929a18b92bea5
MikoRenformed <michaelrenonline@gmail.com>
2020-02-09 22:09:04 +0000

    a

---
+++ .ipynb_checkpoints/TTT-checkpoint.cpp
@@ -0,0 +1,46 @@
+#include "TTT.h"
+
+std::string SquareTypeStringify(SquareType sq){
+  //uses a map to avoid using lots of if if else statements
+  std::map<SquareType, std::string> EmojiMap = {
+    {SquareType::Empty, "\U00002B1C"},
+    {SquareType::X, "\U0000274C"},
+    {SquareType::O, "\U00002B55"},
+  };
+  return EmojiMap[sq];
+}
+
+TTT::TTT(){
+  rows_ = this->get_rows();
+  cols_ = this->get_cols();
+  return;
+}
+
+SquareType TTT::get_square_value(Position pos) const{
+  if( (pos.row >= 0 && pos.row <rows_) && (pos.col >= 0 && pos.col < cols_)){
+    // if the position given is on the board, give that square type.
+    return arr_[pos.row][pos.col];
+  }
+  // otherwise return a wall (so that it won't display as a valid move option)
+  return SquareType::Empty;
+}
+
+void TTT::SetSquareValue(Position pos, SquareType value){
+  if( (pos.row >= 0 && pos.row <rows_) && (pos.col >= 0 && pos.col < cols_)){
+    // if the position given is on the board, set that square;
+    arr_[pos.row][pos.col] = value;
+  }
+  else{
+    std::cout << "Error! Invalid position (not on board)"<<std::endl;
+  }
+  return;
+}
+
+void TTT::CreateBoard(){
+  for(int i = 0; i < rows_; i++){
+    for(int j = 0; j < cols_; j++){
+      Position new_pos = Position(i, j);
+      this->SetSquareValue(new_pos, SquareType::Empty);
+    }
+  }
+}
---
+++ .ipynb_checkpoints/TicTacToe-checkpoint.cpp
@@ -0,0 +1,8 @@
+#include "TTT.h"
+
+int main() {
+  TTT *ttt = new TTT();
+  ttt->CreateBoard();
+  
+  return 0;
+}
