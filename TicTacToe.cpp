db5747ebf615420f1b803feea0e77dd909d9de38
Kayden <kayden.adams@colorado.edu>
2020-02-12 19:25:43 -0700

    Adds Main Runner that runs through 9 turns, alternating players.
    Co-Authored-By: null <MikoRenformed@users.noreply.github.com>

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,8 +3,27 @@
 int main() {
   TTT *ttt = new TTT();
   ttt->CreateBoard();
-
-  ttt->DisplayBoard();
-  Position p = ttt->GetPlayerChoice();
+  SquareType curr_player = SquareType::X;
+  for(int i = 0; i < 9; i++){
+    ttt->DisplayBoard();
+    switch(curr_player){
+      case (SquareType::X):
+        std::cout << "It is currently X's turn.\n";
+        break;
+      case (SquareType::O):
+        std::cout << "It is currently O's turn.\n";
+        break;
+    }
+    Position p = ttt->GetPlayerChoice();
+    ttt->PlaceMarker(p, curr_player);
+    switch(curr_player){
+      case (SquareType::X):
+        curr_player = SquareType::O;
+        break;
+      case (SquareType::O):
+        curr_player = SquareType::X;
+        break;
+    }
+  }
   return 0;
 }
